7c253f96544057e64c396fda10e78714f9abf147
felipe753 <lf.lopez18@gmail.com>
2023-10-17 11:26:07 -0300

    Adding Pi number

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,8 @@
   int main(void) 
  {
       std::cout << "Hello world, I am ready for C++"<<"\n";
+      double pi = 3.14159;
+      cout << "The number PI is equal to "<< pi << endl;
       return 0;
   }
 
