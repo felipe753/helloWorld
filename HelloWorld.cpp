764e7582793bce3e6e8e615a8c8442cdc29315c7
felipe753 <77903785+felipe753@users.noreply.github.com>
2023-10-17 11:03:35 -0300

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+  int main(void) 
+ {
+      std::cout << "Hello world, I am ready for C++"<<"\n";
+      return 0;
+  }
+
+/*
+
+#include <iostream>
+
+ using namespace std;
+ int main()
+ {
+     cout << "Hey, writing std:: is pain, ";
+     cout << "change the program so I don't have to write it.";
+     return 0;
+ }
+
+*/
